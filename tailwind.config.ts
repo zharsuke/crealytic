d0188ba47165738b23a16b6220d9fd56be89c402
zharsuke <alazharisqi@gmail.com>
2025-01-06 00:28:28 +0700

    feat: setup daisyui as usual

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -14,5 +15,8 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: true,
+  }
 } satisfies Config;
